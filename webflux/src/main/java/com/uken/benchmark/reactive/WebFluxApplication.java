19f8fa1a4d1cfb38e240f81e08016718d37e82cb
Kefu Du <kefu@KefudeMacBook-Pro.local>
2018-07-12 13:42:46 -0400

    remove redis test

--- webflux/src/main/java/com/uken/benchmark/reactive/WebFluxApplication.java
+++ webflux/src/main/java/com/uken/benchmark/reactive/WebFluxApplication.java
@@ -2,19 +2,9 @@ package com.uken.benchmark.reactive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
-import org.springframework.data.redis.core.ReactiveRedisTemplate;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
 
 @SpringBootApplication
 public class WebFluxApplication {
-
-  @Bean
-  public ReactiveRedisTemplate<String, String> reactiveRedisTemplate(
-      ReactiveRedisConnectionFactory factory) {
-    return new ReactiveRedisTemplate<>(factory, RedisSerializationContext.string());
-  }
 
   //  @Bean
   //  public ReactiveWebServerFactory reactiveWebServerFactory() {
